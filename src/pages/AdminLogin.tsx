a1cad011bf6ead6941c88be603f784c1bbf7ebe7
GBOUN MARVELLOUS EMMANUEL <94725336+lankiman@users.noreply.github.com>
2025-09-25 01:07:16 +0100

    Update AdminLogin.tsx
    updated remember me on logoin function

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -37,7 +37,7 @@ const AdminLogin = () => {
       password,
       rememberMe,
    }) => {
-      await login({ identifier: email, password, rememberMe });
+      await login({ identifier: email, password, remember_me: rememberMe });
    };
 
    useEffect(() => {
