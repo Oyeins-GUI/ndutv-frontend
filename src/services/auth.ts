a12eb425d46840067c68cd6dd31d6252ee4b2ace
Lankiman <marvellousgboun@gmail.com>
2025-10-02 11:45:44 +0100

    fixed:fixed cookie backend issue

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,42 +2,43 @@ import { BASE_URL } from "@/App";
 import { ApiResponse, LoginPayload, User } from "@/components/AuthProvider";
 
 export async function getUser(): Promise<User | null> {
-   const res = await fetch(`${BASE_URL}/auth/me`, {
-      credentials: "include",
-   });
+  const res = await fetch(`${BASE_URL}/auth/me`, {
+    credentials: "include",
+  });
 
-   if (!res.ok) return null;
+  if (!res.ok) return null;
 
-   const { data }: ApiResponse<User> = await res.json();
-   return data;
+  const { data }: ApiResponse<User> = await res.json();
+  return data;
 }
 
 export async function login(loginData: LoginPayload) {
-   const res = await fetch(`${BASE_URL}/auth/login`, {
-      method: "POST",
-      headers: {
-         "Content-Type": "application/json",
-      },
-      body: JSON.stringify(loginData),
-   });
+  const res = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  });
 
-   if (!res.ok) {
-      const error: ApiResponse<Error> = await res.json();
-      throw new Error(error.message || "Failed to fetch user data");
-   }
+  if (!res.ok) {
+    const error: ApiResponse<Error> = await res.json();
+    throw new Error(error.message || "Failed to fetch user data");
+  }
 
-   const data: ApiResponse<User> = await res.json();
-   return data;
+  const data: ApiResponse<User> = await res.json();
+  return data;
 }
 
 export async function logout() {
-   const res = await fetch(`${BASE_URL}/auth/logout`, {
-      method: "POST",
-      credentials: "include",
-   });
+  const res = await fetch(`${BASE_URL}/auth/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
 
-   if (!res.ok) {
-      const error: ApiResponse<Error> = await res.json();
-      throw new Error(error.message || "Failed to logout user");
-   }
+  if (!res.ok) {
+    const error: ApiResponse<Error> = await res.json();
+    throw new Error(error.message || "Failed to logout user");
+  }
 }
